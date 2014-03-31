a3a688431827245ed86be66d6cfaf81a07df5af4
Randall Lorenz <rlorenz@wisc.edu>
2014-03-31 18:44:02 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -95,4 +95,13 @@ end
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+    
+    it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+    
+describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
